fad5b7019cb3731bb85baafcaf22c40b6145a5f8
Mike Glover <mglover@pobox.com>
2021-03-03 17:46:46 -0700

    handle missing href in a tags

--- wpnngw/article.py
+++ wpnngw/article.py
@@ -64,7 +64,7 @@ class Article(object):
 		if social_footer:
 			social_footer.replaceWith('')
 		for a in soup('a'):
-			if a.text != a.attrs['href']:
+			if 'href' in a and a.text != a.attrs['href']:
 				a.replaceWith("%s (%s)" % (a.text, a.attrs['href']))
 		for b in soup('b'):
 			b.replaceWith('*%s*'%b.text)
