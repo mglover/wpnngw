bae4f50e303e10f3572ae9337b3d69408d69ec2b
Mike Glover <mglover@pobox.com>
2021-03-11 10:41:53 -0700

    gwmail only queues, does not send

--- bin/gwmail.py
+++ bin/gwmail.py
@@ -4,23 +4,21 @@
 	and post those articles through the blog's approval system
 """
 
-import sys, subprocess
+import os, sys, subprocess
 from datetime import datetime
+from wpnngw.util import inn_config, QueueDir
 
-lf = open('/var/spool/news/wpnngw/gwmail.log', 'w')
-def log(msg):
-	lf.write("%s: %s\n" %  (datetime.isoformat(datetime.now()), msg))
-	lf.flush()
-
+QROOT=os.path.join(inn_config()['pathspool'],'wpnngw_incoming')
 
 if __name__ == '__main__':
 	addresses = sys.argv[1:]
-	log("received for %s" % addresses)
 	if addresses and addresses[0].endswith('@wpnngw.local'):
-		log("post_comment")
-		ret = subprocess.run(['/var/spool/news/wpnngw/bin/post_comment.py', '--'])
-		log("post_comment returned %d" % ret.returncode)
-		sys.exit(ret.returncode)
+		group = addresses[0].rstrip('@wpnngw.local')
+		now = datetime.timestamp(datetime.now())
+		qfile = QueueDir(QROOT).newfile("%s-%s" % (group, now))
+		with open(qfile, 'w') as qfd: qfd.write(sys.stdin.read())
+		qfd.close()
+
 	elif addresses[0] == 'usenet':
 		# innd initialization?
 		sys.stderr.write("gwmail initialized\n")
